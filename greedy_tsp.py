d29929af26064da3c96d46c068ed65e269948bda
nathanielnowel <nnowel23@gmail.com>
2023-03-23 16:51:41 -0500

    Greedy working solution

--- greedy_tsp.py
+++ greedy_tsp.py
@@ -15,21 +15,58 @@ def tsp_greedy(name_file, distance_file):
         distances_list.append(row)
 
         # turn strings into floats
-        for list in distances_list:
-            for str in range(len(list)):
-                list[str] = float(list[str])
+        for lst in distances_list:
+            for str in range(len(lst)):
+                lst[str] = float(lst[str])
 
     # i = rows, j = columns
-    i = 0
-    j = -1
-    final_distance_list = [0]
-    for lst in distances_list:
-        minimum = min(lst)
-        for dist in lst:
-            j += 1
-            if dist == minimum:
-                final_distance_list.append(j)
+    row = 0
+    column = -1
+    final_distance_list = []
+    final_location_list = [0]
+
+    # current_city = 0
+
+    for lst in range(len(distances_list) - 1):
+        minimum = max(distances_list[row])  # initial minimum value
+        for distance in distances_list[row]:
+
+            # for next_city in range(len(distances_list[current_city])):
+
+            column += 1
+            # print(distance)
+            # print(minimum)
+            # print(row)
+            # print(column)
+            # print()
+
+            if 0 < distance <= minimum and column not in final_location_list:
+                minimum = distance
+                location = column
+                # print(f"minimum: {minimum}")
+                # print(f"location: {location}")
+                # print()
+            if column == len(distances_list[row]) - 1:
+                final_location_list.append(location)
+                final_distance_list.append(minimum)
+                row = location
+                column = -1
+
+    # print(final_location_list)
+    # print(final_distance_list)
+
+    final_city_list = []
+    for city in final_location_list:
+        final_city_list.append(city_dict[city])
+
+    print(final_city_list)
+
+    distance_sum = 0
+    for distance in final_distance_list:
+        distance_sum += distance
+
+    print(f"Total Distance: {distance_sum}")
 
 
-tsp_greedy("/Users/nathanielnowel/PycharmProjects/travellingsalesperson/seven_cities_names.txt",
-           "/Users/nathanielnowel/PycharmProjects/travellingsalesperson/seven_cities_distances.txt")
+tsp_greedy("/Users/nathanielnowel/PycharmProjects/travellingsalesperson/thirty_cities_names.txt",
+           "/Users/nathanielnowel/PycharmProjects/travellingsalesperson/thirty_cities_distances.txt")
